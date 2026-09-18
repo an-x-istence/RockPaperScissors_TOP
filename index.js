d4e252d1add44fc01893fc21836b2786e37adce8
an-x-istence <114814137+an-x-istence@users.noreply.github.com>
2026-09-18 20:24:58 +0100

    Write funtion to get computer choice following pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Bonvoyage")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerThrow;
+    if (randomNumber == 1) {
+        computerThrow = "rock";
+    } else if (randomNumber == 2) {
+        computerThrow = "paper"
+    } else {
+        computerThrow = "scissors"
+    }
+
+    return computerThrow
+}
+
+function getHumanChoice() {
+    let humanThrow = prompt("Will you throw rock, paper, or scissors? (entries all in lowercase)")
+    // return humanThrow
+    console.log(humanThrow)
+}
+
+getHumanChoice()
