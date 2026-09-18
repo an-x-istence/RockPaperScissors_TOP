9389c3585952d167bed6729785bc6bbc91b159c9
an-x-istence <114814137+an-x-istence@users.noreply.github.com>
2026-09-18 23:30:57 +0100

    Write funtion logic for play round according to psuedocode

--- index.js
+++ index.js
@@ -20,3 +20,52 @@ function getHumanChoice() {
     return humanThrow
 }
 
+function playRound(a = getHumanChoice, b = getComputerChoice) {
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+    if (computer == human) {
+        computerScore += 0;
+        userScore += 0;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore)
+        console.log("User's score: " + userScore)
+    } else if (human == "rock" && computer == "paper") {
+        computerScore += 1;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    } else if (human == "scissors" && computer == "paper") {
+        userScore += 1;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    }  else if (human == "rock" && computer == "scissors") {
+        userScore += 1;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    } else if (human == "paper" && computer == "scissors") {
+        computerScore += 1;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    } else if (human == "paper" && computer == "rock") {
+        userScore += 1;
+        console.log("The computer threw: " + computer);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    } else if (human == "scissors" && computer == "rock") {
+        computerScore += 1;
+        console.log("The computer threw: " + getComputerChoice);
+        console.log("You threw: " + human);
+        console.log("Computer's score: " + computerScore);
+        console.log("User's score: " + userScore);
+    }
+}
+
